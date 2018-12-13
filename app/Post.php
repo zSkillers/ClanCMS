3c8154a8d652008a01cd2402dd02d0527f5f3f94
Cody Wofford <cody.e.wofford@gmail.com>
2018-12-12 22:05:44 -0500

    Added belong to thread and user.

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+  public function thread()
+  {
+    $this->belongsTo('App\Thread');
+  }
+
+  public function user()
+  {
+    $this->belongsTo('App\User');
+  }
 }
