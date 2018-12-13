adc6fe73cd4baaca74924af3991a1edf542ba4c5
Cody Wofford <cody.e.wofford@gmail.com>
2018-12-12 22:05:05 -0500

    Added findByForum function to get forums by ID for API.

--- app/Http/Controllers/API/ForumController.php
+++ app/Http/Controllers/API/ForumController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Forum as Forum;
+use App\Thread as Thread;
 
 class ForumController extends Controller
 {
@@ -20,6 +21,12 @@ class ForumController extends Controller
 
   public function findByCategoryId(Request $request) {
     return Forum::where('category_id', $request['category_id'])->paginate(5);
+  }
+
+  public function findByForumID(Request $request)
+  {
+    $threads = Forum::where('id', $request['forum_id'])->with('threads', 'threads.user')->get();
+    return $threads;
   }
 
   public function findByCategoryIdFunc($id) {
