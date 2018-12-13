dc8e9d57fa6b7055af873fd1e78390e44d18d8b3
Cody Wofford <cody.e.wofford@gmail.com>
2018-12-12 22:06:21 -0500

    Added belong to Forum and User. Added one:many for threads:post.

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+  public function forum()
+  {
+    return $this->belongsTo('App\Forum');
+  }
+
+  public function threads()
+  {
+    return $this->hasMany('App\Post');
+  }
+
+  public function user()
+  {
+    return $this->belongsTo('App\User');
+  }
 }
