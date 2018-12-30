095723c0cc34ff1ee5b46e4ac6c5f3f163c0f72c
Cody Wofford <cody.e.wofford@gmail.com>
2018-12-29 22:16:31 -0500

    PostBit Component

---
+++ resources/assets/js/components/Forum/PostBit.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="direct-chat-msg">
+    <div class="direct-chat-info clearfix">
+        <span class="direct-chat-name pull-left">{{username}}</span>
+        <span class="direct-chat-timestamp pull-right">{{date}}</span>
+    </div><!-- /.direct-chat-info -->
+    <img class="direct-chat-img" :src='avatar' alt="message user image">
+    <!-- /.direct-chat-img -->
+    <div class="direct-chat-text">
+        {{message}}
+    </div><!-- /.direct-chat-text -->
+  </div><!-- /.direct-chat-msg -->
+</template>
+
+<script>
+export default {
+  props: ['username', 'avatar', 'message', 'date']
+}
+</script>
