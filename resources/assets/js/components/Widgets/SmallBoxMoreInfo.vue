fcc880e84577703400d7a36fb67bc95ebe0261d3
Cody Wofford <cody.e.wofford@gmail.com>
2018-12-29 22:17:00 -0500

    Small Box With More Info Link Component.

---
+++ resources/assets/js/components/Widgets/SmallBoxMoreInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="small-box shadow" :class='color'>
+    <div class="inner">
+      <h3>{{header}}</h3>
+      <p>{{body}}</p>
+    </div>
+    <div class="icon">
+      <i :class='icon'></i>
+    </div>
+    <router-link :to='url' class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['header', 'body', 'url', 'icon', 'color']
+}
+</script>
+
+<style>
+
+</style>
