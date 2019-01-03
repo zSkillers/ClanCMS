beaaa09e820f45cefbd65ab628b4d660f2b53dcd
Cody Wofford <cody.e.wofford@gmail.com>
2019-01-03 15:54:21 -0500

    Added Fillables for the thread.

--- app/Thread.php
+++ app/Thread.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'id', 'title', 'user_id', 'forum_id'
+  ];
+
   public function forum()
   {
     return $this->belongsTo('App\Forum');
