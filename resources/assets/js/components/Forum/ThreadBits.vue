cdf6c47ad123da13dce1f8227f0f36e22b5bc48a
Cody Wofford <cody.e.wofford@gmail.com>
2019-01-03 15:56:57 -0500

    Added threadbits component.

---
+++ resources/assets/js/components/Forum/ThreadBits.vue
@@ -0,0 +1,27 @@
+<template>
+    <tr>
+      <td>{{ title }}</td>
+      <td>{{ user.name }}</td>
+      <td>{{ replycount }}</td>
+      <td>Last Reply Here</td>
+    </tr><!-- /.tr -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {name:'Loading..'},
+    }
+  },
+  props: ['title', 'userid', 'replycount'],
+  created() {
+    axios.get(this.$site_url_address + 'api/user/' + this.userid)
+    .then(response => {this.user = response.data});
+  }
+}
+</script>
+
+<style>
+
+</style>
