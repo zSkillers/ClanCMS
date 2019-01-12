80d95c9cb1623e58e5cc912ec993faa4d5a5d309
Cody Wofford <cody.e.wofford@gmail.com>
2019-01-11 20:55:33 -0500

    Post get API.

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Post as Post;
 
 class PostController extends Controller
 {
-    //
+
+  public function findPostsByThreadId(Request $request)
+  {
+    $posts = Post::where('thread_id', $request['thread_id'])->with('user', 'thread');
+    return $posts->paginate(18);
+  }
+
 }
