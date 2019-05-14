9651087ef17a16e42b2923009fd1800886037cb2
Cody Wofford <cody.e.wofford@gmail.com>
2019-05-13 23:25:22 -0400

    Layout update.

--- resources/assets/js/components/Dashboard.vue
+++ resources/assets/js/components/Dashboard.vue
@@ -1,116 +1,114 @@
 <template>
-  <div>
-        <div v-if="$gate.isAdminOrAuthor()">
-          <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Widgets</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Widgets</li>
-            </ol>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-4 col-6">
-            <SmallBoxMoreInfo
-            v-bind:header='widgetInfo.userCount'
-            body='Users'
-            url='/users'
-            icon='ion ion-ios-people'
-            color='bg-success-gradient'
-            ></SmallBoxMoreInfo>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-4 col-6">
-            <SmallBoxMoreInfo
-            v-bind:header='widgetInfo.categoryCount'
-            body='Category'
-            url='/category'
-            icon='ion ion-locked'
-            color='bg-primary-gradient'
-            ></SmallBoxMoreInfo>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-4 col-6">
-            <SmallBoxMoreInfo
-            v-bind:header='widgetInfo.forumCount'
-            body='Forum'
-            url='/category'
-            icon='ion ion-pie-graph'
-            color='bg-danger-gradient'
-            ></SmallBoxMoreInfo>
-          </div>
+<div>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Dashboard
+            <small>Daily digest of statistical data.</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active">Widgets</li>
+        </ol>
+    </section>
 
-        </div>
-        <div class="row">
-          <div class="col-lg-4 col-6">
-            <SmallBoxMoreInfo
-            v-bind:header='widgetInfo.categoryCount'
-            body='Threads'
-            url='/users'
-            icon='ion ion-ios-people'
-            color='bg-warning'
-            ></SmallBoxMoreInfo>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-4 col-6">
-            <SmallBoxMoreInfo
-            v-bind:header='widgetInfo.forumCount'
-            body='Posts'
-            url='/category'
-            icon='ion ion-chatbubble-working'
-            color='bg-info'
-            ></SmallBoxMoreInfo>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-4 col-6">
-            <SmallBoxMoreInfo
-            v-bind:header='widgetInfo.userCount'
-            body='Staff Online'
-            url='/category'
-            icon='ion ion-arrow-graph-up-right'
-            color='bg-danger'
-            ></SmallBoxMoreInfo>
-          </div>
-          <!-- ./col -->
-        </div>
-        </div>
+    <!-- Main content -->
+    <div v-if="$gate.isAdminOrAuthor()">
+        <section class="content">
+            <div class="row">
+                <div class="col-lg-4 col-6">
+                    <SmallBoxMoreInfo
+                        v-bind:header='widgetInfo.userCount'
+                        body='Users' url='/users'
+                        icon='ion ion-ios-people'
+                        color='bg-green'>
+                    </SmallBoxMoreInfo>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-4 col-6">
+                    <SmallBoxMoreInfo
+                        v-bind:header='widgetInfo.categoryCount'
+                        body='Category' url='/category'
+                        icon='ion ion-locked'
+                        color='bg-aqua'>
+                    </SmallBoxMoreInfo>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-4 col-6">
+                    <SmallBoxMoreInfo
+                        v-bind:header='widgetInfo.forumCount'
+                        body='Forum' url='/category'
+                        icon='ion ion-pie-graph'
+                        color='bg-yellow'>
+                    </SmallBoxMoreInfo>
+                </div>
 
-        <div v-if="!$gate.isAdminOrAuthor()">
-            <not-found></not-found>
-        </div>
-      </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-6">
+                        <SmallBoxMoreInfo
+                            v-bind:header='widgetInfo.categoryCount'
+                            body='Threads' url='/users'
+                            icon='ion ion-ios-people'
+                            color='bg-red'>
+                        </SmallBoxMoreInfo>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-4 col-6">
+                        <SmallBoxMoreInfo
+                            v-bind:header='widgetInfo.forumCount'
+                            body='Posts' url='/category'
+                            icon='ion ion-chatbubble-working'
+                            color='bg-purple'>
+                        </SmallBoxMoreInfo>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-4 col-6">
+                        <SmallBoxMoreInfo
+                            v-bind:header='widgetInfo.userCount'
+                            body='Staff Online' url='/category'
+                            icon='ion ion-arrow-graph-up-right'
+                            color='bg-teal'>
+                        </SmallBoxMoreInfo>
+                    </div>
+                    <!-- ./col -->
+            </div>
+        </section>
+    </div>
+
+    <div v-if="!$gate.isAdminOrAuthor()">
+        <not-found></not-found>
+    </div>
+</div>
 </template>
 
 <script>
 import SmallBoxMoreInfo from './Widgets/SmallBoxMoreInfo.vue'
-    export default {
-      data() {
-          return {
-              widgetInfo : ''
-          }
-      },
-      components: {
+export default {
+    data() {
+        return {
+            widgetInfo: ''
+        }
+    },
+    components: {
         SmallBoxMoreInfo
-      },
-      mounted() {
+    },
+    mounted() {
         console.log('Component mounted.')
         this.loadSetting();
-      },
-      methods: {
+    },
+    methods: {
         loadSetting() {
-          axios.get(this.$site_url_address + 'api/dashboard').then(response => {
-            this.widgetInfo = response.data;
-            console.log(this.widgetInfo);
-                      console.log(this.widgetInfo.userCount);
-          });
+            axios.get(this.$site_url_address + 'api/dashboard').then(response => {
+                this.widgetInfo = response.data;
+                console.log(this.widgetInfo);
+                console.log(this.widgetInfo.userCount);
+            });
         },
         created() {
-           this.loadSetting();
+            this.loadSetting();
             //setInterval(() => this.loadSetting(), 3000);
         }
-      }
     }
+}
 </script>
