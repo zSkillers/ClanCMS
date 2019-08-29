0073ef5b96fb560b58994e8abb22764118369936
Cody Wofford <cody.e.wofford@gmail.com>
2019-08-28 22:24:36 -0400

    More widget counters.

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -8,14 +8,18 @@ use App\User as User;
 use App\Category as Category;
 use App\Forum as Forum;
 use App\Dashboard as Dashboard;
+use App\Post as Post;
+use App\Thread as Thread;
 
 class DashboardController extends Controller
 {
     public function index() {
       $dashboardData = array(
-        'userCount' => User::count(),
-        'categoryCount' => Category::count(),
-        'forumCount' => Forum::count()
+        'user_count' => User::count(),
+        'category_count' => Category::count(),
+        'forum_count' => Forum::count(),
+        'thread_count' => Thread::count(),
+        'post_count' => Post::count()
       );
       return json_encode($dashboardData);
     }
