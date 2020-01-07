b09ed2f7f066d3b1e79485ed658fc9be0b87bd96
Cody Wofford <cody.e.wofford@gmail.com>
2020-01-06 21:59:28 -0500

    Changed Syntax Style

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\TrustProxies::class
     ];
 
     /**
@@ -35,13 +35,10 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class
         ],
 
-        'api' => [
-            'throttle:60,1',
-            'bindings',
-        ],
+        'api' => ['throttle:60,1', 'bindings']
     ];
 
     /**
@@ -53,12 +50,13 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.basic' =>
+            \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class
     ];
 }
--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,42 +12,43 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-      for($i = 0; $i < 10; $i++) {
-        DB::table('categories')->insert([
-          'title' => str_random(10),
-          'description' => str_random(10),
-          'sort' => 1,
-          'created_at' => \DB::raw('CURRENT_TIMESTAMP'),
-          'updated_at' => \DB::raw('CURRENT_TIMESTAMP')
-        ]);
-
-        for($j = 0; $j < 10; $j++) {
-          DB::table('forums')->insert([
-            'title' => str_random(10),
-            'category_id' => $i+1,
-            'created_at' => \DB::raw('CURRENT_TIMESTAMP'),
-            'updated_at' => \DB::raw('CURRENT_TIMESTAMP')
-          ]);
-
-          for($q = 0; $q < 10; $q++) {
-            DB::table('threads')->insert([
-              'title' => str_random(10),
-              'user_id' => $j+1,
-              'forum_id' => $j+1,
-              'created_at' => \DB::raw('CURRENT_TIMESTAMP'),
-              'updated_at' => \DB::raw('CURRENT_TIMESTAMP')
-            ]);
-            for($w = 0; $w < 10; $w++) {
-              DB::table('posts')->insert([
-                'user_id' => $w+1,
-                'thread_id' => $q+1,
-                'body' => "This is a hello world test!",
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('categories')->insert([
+                'title' => str_random(10),
+                'description' => str_random(10),
+                'sort' => 1,
                 'created_at' => \DB::raw('CURRENT_TIMESTAMP'),
                 'updated_at' => \DB::raw('CURRENT_TIMESTAMP')
-              ]);
+            ]);
+
+            for ($j = 0; $j < 10; $j++) {
+                DB::table('forums')->insert([
+                    'title' => str_random(10),
+                    'category_id' => $i + 1,
+                    'created_at' => \DB::raw('CURRENT_TIMESTAMP'),
+                    'updated_at' => \DB::raw('CURRENT_TIMESTAMP')
+                ]);
+
+                for ($q = 0; $q < 10; $q++) {
+                    DB::table('threads')->insert([
+                        'title' => str_random(10),
+                        'user_id' => $j + 1,
+                        'forum_id' => $j + 1,
+                        'created_at' => \DB::raw('CURRENT_TIMESTAMP'),
+                        'updated_at' => \DB::raw('CURRENT_TIMESTAMP'),
+                        'last_user_reply_id' => $j + 1
+                    ]);
+                    for ($w = 0; $w < 10; $w++) {
+                        DB::table('posts')->insert([
+                            'user_id' => $w + 1,
+                            'thread_id' => $q + 1,
+                            'body' => "This is a hello world test!",
+                            'created_at' => \DB::raw('CURRENT_TIMESTAMP'),
+                            'updated_at' => \DB::raw('CURRENT_TIMESTAMP')
+                        ]);
+                    }
+                }
             }
-          }
         }
-      }
     }
 }
